b265661c124092dddaa2f79713134a9672cd7cee
ravinderchautala <ryvinder@gmail.com>
2022-06-14 12:42:15 +0530

    first commit

---
+++ yahoo_baba/ch_3_html-tags-in-js/html-tags.js
@@ -0,0 +1,2 @@
+document.write("<b>Hello i am italic tag through JavaScript</b><br>");
+document.write("<i>Hello world</i>");
---
+++ yahoo_baba/ch_4_comments/comments.js
@@ -0,0 +1,3 @@
+document.write("<b>Hello i am italic tag through JavaScript</b><br>");
+// document.write("<i>Hello world</i>");
+/*hello world i am learning JavaScript from Yahoo Baba*/
