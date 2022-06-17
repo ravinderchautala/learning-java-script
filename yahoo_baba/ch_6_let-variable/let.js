e039ea5d2ed5e53b2039a9f1c90c1de3603d1c9c
ravinderchautala <ryvinder@gmail.com>
2022-06-17 11:41:55 +0530

    let and const variables tutorial completed

---
+++ yahoo_baba/ch_6_let-variable/let.js
@@ -0,0 +1,10 @@
+let z="hello world ";
+var x="Ravinder";
+var x="Ravinder ";
+z ="hello guys "
+// constant variable
+const second= "hello";
+// second="world";
+document.write(z);
+document.write(x);
+document.write(second);
