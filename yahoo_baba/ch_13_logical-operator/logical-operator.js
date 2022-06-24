e6fa279a3809ee905fa4b90fbec15d40abfa23f4
ravinderchautala <ryvinder@gmail.com>
2022-06-24 22:25:44 +0530

    if else statement tutorial completed

--- yahoo_baba/ch_13_logical-operator/logical-operator.js
+++ yahoo_baba/ch_13_logical-operator/logical-operator.js
@@ -2,13 +2,13 @@ var age= 20;
 
 
 if(age>=18 && age<=21){
-    document.write("Yes you are eligible!!");
+    document.log("Yes you are eligible!!");
 }
 var a= 23;
 var b= 21;
 if(a>=b || a<=b){
-    document.write("Yes you are eligible!!");
+    document.log("Yes you are eligible!!");
 }
 if (!(age<=18)){
-    document.write("Yes you are eligible!!  ");
+    document.log("Yes you are eligible!!  ");
 }
---
+++ yahoo_baba/ch_14_ifelse/ifelse.js
@@ -0,0 +1,8 @@
+var x=55;
+
+if(x>30){
+    document.write("X is Greater.");
+}
+else{
+    document.write("X is smaller");
+}
