f85784232eba6f0195b83033288aa1456a76d69a
ravinderchautala <ryvinder@gmail.com>
2022-06-24 22:45:45 +0530

    if else if tutorial completed

---
+++ yahoo_baba/ch_15_ifelseif/ifelseif.js
@@ -0,0 +1,13 @@
+var pr=65;
+
+if(pr>=80 && pr<=100){
+    document.write("You are in Merit.");
+}else if(pr>=60 && pr<=80){
+    document.write("You are in Ist Division.");
+}else if(pr>=45 && pr<=60){
+    document.write("You are in IInd Division.");
+}else if(pr>=33 && pr<=45){
+    document.write("You are in IIIrd Division.");
+}else{
+    document.write("You are Fail.");
+}
