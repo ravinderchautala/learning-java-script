c566ee2e25b587ffb9adea2b72ad4cfc407087ba
ravinderchautala <ryvinder@gmail.com>
2022-06-28 15:38:40 +0530

    functions with parameters tutorial completed

---
+++ yahoo_baba/ch_22_functions-with-para/functions-with-para.js
@@ -0,0 +1,10 @@
+function name(first="Ravinder",second="Chautala"){
+    document.write("fullname is " +  first + " " + second+"<br>");
+}
+function sum(a,b){
+    document.write(a+b);
+}
+name("Vinita","Rahar");
+name("Vinita","Ravinder");
+name();
+sum(24,45);
