417fc43d0e99673a2cdb70952b3632c275e6f017
ravinderchautala <ryvinder@gmail.com>
2022-06-28 15:00:32 +0530

    confirm box tutorial completed

--- yahoo_baba/ch_18_alert-box/alert-box.js
+++ yahoo_baba/ch_18_alert-box/alert-box.js
@@ -1,1 +1,2 @@
+// you can show anything in alert box any function or any calculation.
 alert("Allow cookies");
---
+++ yahoo_baba/ch_19_confirm-box/confirm-box.js
@@ -0,0 +1,6 @@
+var a = confirm("Do you like our website?");
+if (a) {
+    alert("Thank You.");
+} else {
+    alert("Sorry.");
+}
