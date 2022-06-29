880ab77f9e14d9669f4b974f9998d596d0f7a57c
ravinderchautala <ryvinder@gmail.com>
2022-06-30 01:27:28 +0530

    video 27 28 29 30 completed

---
+++ yahoo_baba/ch_27_do-while/do-while.js
@@ -0,0 +1,5 @@
+var a = 1;
+do{
+    document.write(a +" vinita<br>");
+    a++;
+}while(a<=10);
---
+++ yahoo_baba/ch_29_break-&-continue/break&continue.js
@@ -0,0 +1,8 @@
+for(var a=1;a<=10;a++){
+    if(a==3){
+        document.write("RAvinder "+ a + "<br>");
+        // continue;
+        break;
+    }
+    document.write(a+" vinita<br>")
+}
