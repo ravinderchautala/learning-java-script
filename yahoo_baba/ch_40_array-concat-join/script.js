a1e6252d79e3a0e4d925f001caabdf9127ad2935
ravinderchautala <ryvinder@gmail.com>
2022-07-03 23:41:05 +0530

    array concat and join function tutorial completed

---
+++ yahoo_baba/ch_40_array-concat-join/script.js
@@ -0,0 +1,6 @@
+var a = ["Ravinder", "Vinita","Jatta","Anu"];
+var b = ["Ravinder", "Vinita","Jatta","Anu"];
+var c = a.concat(b);
+document.write(c+"<br>");
+var d=a.join("-");
+document.write(d+"<br>");
