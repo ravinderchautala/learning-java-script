b14249fb7c58844765e96745a2d1906e2f44e6ef
ravinderchautala <ryvinder@gmail.com>
2022-07-03 23:02:30 +0530

    array modification tutorial completed

---
+++ yahoo_baba/ch_36_array-modification/script.js
@@ -0,0 +1,10 @@
+var a = ["Vinita",23, "Female","M.sc"];
+document.write(a+"<br>");
+a[0] ="Anu";
+document.write(a+"<br>");
+a[1]= 25 ;
+document.write(a+"<br>");
+delete a[3];
+document.write(a+"<br>");
+
+
