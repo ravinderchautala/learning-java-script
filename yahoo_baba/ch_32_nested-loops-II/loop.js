cb70af521c5db8e1e487996cbb8cac6c1d744206
ravinderchautala <ryvinder@gmail.com>
2022-07-03 12:39:25 +0530

    nested loops II tutorial completed

---
+++ yahoo_baba/ch_32_nested-loops-II/loop.js
@@ -0,0 +1,27 @@
+for( var a = 1; a<=5 ;a++){
+    for(var b = 1; b <= a; b++){
+        document.write(b+" ");
+    }
+    document.write("<br>");
+}
+document.write("<br>");
+for( var c = 1; c<=5 ;c++){
+    for(var d = 1; d <= c; d++){
+        document.write(c+" ");
+    }
+    document.write("<br>");
+}
+document.write("<br>");
+for( var g = 5; g>=1 ;g--){
+    for(var h = g; h >= 1; h--){
+        document.write(g+" ");
+    }
+    document.write("<br>");
+}
+document.write("<br>");
+for( var e = 5; e>=1 ;e--){
+    for(var f = e; f >= 1; f--){
+        document.write(f+" ");
+    }
+    document.write("<br>");
+}
