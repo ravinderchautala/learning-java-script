d4c9c77dd6a4cd657d9bb011c70d0843f57dc178
ravinderchautala <ryvinder@gmail.com>
2022-07-04 07:07:24 +0530

    array slice and splice functions tutorial completed

---
+++ yahoo_baba/ch_41_array-slice-splice/script.js
@@ -0,0 +1,9 @@
+var a = ["Ravinder", "Vinita","Jatta","Anu"];
+var b = ["Ravinder", "Vinita","Jatta","Anu"];
+document.write(b+"<br>");
+var c = b.slice(1, 4);
+document.write(c+"<br>");
+a.splice(2,1,"RAvivini","dnfojgd")
+document.write(a+"<br>");
+
+
