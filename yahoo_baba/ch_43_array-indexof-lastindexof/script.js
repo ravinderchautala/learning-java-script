24da34efe394f13e231231de9251febd9244b610
ravinderchautala <ryvinder@gmail.com>
2022-07-04 07:21:22 +0530

    indexOf and lastIndexOf function tutorial completed

---
+++ yahoo_baba/ch_43_array-indexof-lastindexof/script.js
@@ -0,0 +1,6 @@
+var a = ["Ravinder", "Vinita","Jatta","Anu"];
+document.write(a+"<br>");
+var b = a.indexOf("Vinita")
+document.write(b+"<br>");
+var c = a.lastIndexOf("Anu");
+document.write(c+"<br>");
