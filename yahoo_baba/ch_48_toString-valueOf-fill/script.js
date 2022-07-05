d5cf3b6c21bbd148af70ead5f2c564e82e32b8bb
ravinderchautala <ryvinder@gmail.com>
2022-07-05 23:33:26 +0530

    array toString(),valueOf() and fill() functions tutorial completed

---
+++ yahoo_baba/ch_48_toString-valueOf-fill/script.js
@@ -0,0 +1,9 @@
+var a =["Ravinder","Vinita","Jatta","Anu"];
+// toString
+a.toString();
+document.write(a+"<br>");
+// valueOf: returns the value of complete array
+document.write(a.valueOf()+"<br>");
+// fill
+a.fill("ram")
+document.write(a+"<br>");
