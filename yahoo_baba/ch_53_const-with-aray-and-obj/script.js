813f4c10ca2f7d225fac422503a4a482229e38ec
ravinderchautala <ryvinder@gmail.com>
2022-07-07 14:23:48 +0530

    const with array and object tutorial completed

---
+++ yahoo_baba/ch_53_const-with-aray-and-obj/script.js
@@ -0,0 +1,11 @@
+const a =[10,20,30];
+// a=30; 
+a[1]=25;
+// a = [40,50,60];
+document.write(a+ "<br>");
+const b ={
+    name: 'vini',
+    age: 25
+};
+b.name = 'vinita';
+document.write(b.name);
