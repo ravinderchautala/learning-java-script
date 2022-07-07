51abbb0feec6ba76f1f211e9a7d71f7ea08cc86c
ravinderchautala <ryvinder@gmail.com>
2022-07-07 15:30:51 +0530

    string mathods II tutorial completed

---
+++ yahoo_baba/ch_57_string-methods-II/script.js
@@ -0,0 +1,27 @@
+var str = "JavaScript is a great language";
+var str2 = "Java and ";
+var x = 50 ;
+
+var a = str.charAt(3);
+var b = str.charCodeAt(2);
+var c = String.fromCharCode(65);
+var d = str2.concat(str);
+var e = str.split(" ");
+var f = str.repeat(2);
+var g = str.slice(4,10);
+var h = str.substr(4,6);
+var i = str.substring(0,11);
+var j = x.toString();
+var k = str.valueOf();
+
+document.write(a+"<br>");
+document.write(b+"<br>");
+document.write(c+"<br>");
+document.write(d+"<br>");
+document.write(e+"<br>");
+document.write(f+"<br>");
+document.write(g+"<br>");
+document.write(h+"<br>");
+document.write(i+"<br>");
+document.write(j+"<br>");
+document.write(k+"<br>");
