52915b13471ce83f85943799fee092bfa5ec5d4f
ravinderchautala <ryvinder@gmail.com>
2022-07-07 14:11:19 +0530

    array of object tutorial completed

---
+++ yahoo_baba/ch_52_array-of-object/script.js
@@ -0,0 +1,10 @@
+var student = [
+    {name:'vinita',age : 25},
+    {name:'Ravinder',age : 23},
+    {name:'Anu',age : 25},
+    {name:'Jatta',age : 23},
+];
+console.log();
+for (var  a=0;a < student.length; a++){
+    document.write(student[a].name+" " + "<br>")
+}
