52af9672219a77f6c9c74224752f32c3bab5fa2d
ravinderchautala <ryvinder@gmail.com>
2022-07-07 16:15:26 +0530

    math mathods tutorial completed

---
+++ yahoo_baba/ch_59_math-methods/script.js
@@ -0,0 +1,16 @@
+var num = 45.65;
+var num1 = -1.25;
+
+document.write(Math.ceil(num) + "<br>");
+document.write(Math.floor(num) + "<br>");
+document.write(Math.ceil(num1) + "<br>");
+document.write(Math.floor(num1) + "<br>");
+document.write(Math.round(num) + "<br>");
+document.write(Math.max(-8,49,-37,5,67) + "<br>");
+document.write(Math.min(-8,49,-37,5,67) + "<br>");
+document.write(Math.sqrt(225) + "<br>");
+document.write(Math.cbrt(125) + "<br>");
+document.write(Math.pow(3,5) + "<br>");
+document.write(Math.floor(Math.random() * 10) + 1 + "<br>");
+document.write(Math.abs(-2.25) + "<br>");
+document.write(Math.PI + "<br>");
