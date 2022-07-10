ee55d274b02097ab256ec7a658dec0d60d0bf715
ravinderchautala <ryvinder@gmail.com>
2022-07-10 15:34:38 +0530

    DOM targeting metgods tutorial completed

---
+++ yahoo_baba/ch_62_DOM-targeting-methods/script.js
@@ -0,0 +1,17 @@
+var element;
+
+// element = document;
+// element = document.all;
+// element = document.all[6];
+// element = document.title;
+// element = document.body;
+// element = document.links;
+// element = document.images;
+// element = document.URL;
+// element = document.domain;
+// element = document.baseURI;
+// element = document.getElementById("sidebar");
+// element = document.getElementsByClassName("center");
+element = document.getElementsByTagName("ul");
+
+console.log(element);
