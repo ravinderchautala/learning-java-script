35e0ca08fc767f221e630fd9c754a6b2dbbd2592
ravinderchautala <ryvinder@gmail.com>
2022-07-12 14:28:08 +0530

    query selector and query selectorall tutorial completed

---
+++ yahoo_baba/ch_64_queryselector-queryselectorall/script.js
@@ -0,0 +1,7 @@
+var element;
+
+document.querySelector("#header").innerHTML = "<h1>Wow</h1>"
+element = document.querySelector(".list");
+console.log(element);
+element = document.querySelectorAll(".list");
+console.log(element);
