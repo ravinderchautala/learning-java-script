3978b1286c6cf64e8e2deecf43742bae610f1b4a
ravinderchautala <ryvinder@gmail.com>
2022-07-14 11:45:14 +0530

    traversal methods III tutorial completed

---
+++ yahoo_baba/ch_70_traversal-methods-III/script.js
@@ -0,0 +1,22 @@
+// firstElementChild
+document.getElementById("inner").firstElementChild.style.backgroundColor = "brown";
+
+// lastElementChild
+document.getElementById("inner").lastElementChild.style.backgroundColor = "black";
+
+// lastElementChild
+var a = document.getElementById("inner").lastElementChild;
+
+// firstElementChild
+var b = document.getElementById("inner").firstElementChild.innerHTML;
+
+// firstChild
+var c = document.getElementById("inner").firstChild;
+
+// lastChild
+var d = document.getElementById("inner").lastChild;
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
