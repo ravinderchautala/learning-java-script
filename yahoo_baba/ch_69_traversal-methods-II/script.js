d4c497af7fc860cd1616ac5cc900c0628f147e7b
ravinderchautala <ryvinder@gmail.com>
2022-07-14 11:31:48 +0530

    traversal methods II tutorial completed

---
+++ yahoo_baba/ch_69_traversal-methods-II/script.js
@@ -0,0 +1,9 @@
+document.getElementById("inner").children[1].style.backgroundColor = "green";
+
+document.getElementById("inner").childNodes[5].style.backgroundColor = "black";
+
+var a = document.getElementById("main").children;
+var b = document.getElementById("inner").childNodes;
+
+console.log(a);
+console.log(b);
