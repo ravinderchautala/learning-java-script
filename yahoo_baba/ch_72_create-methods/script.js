b0d238dcf68afba6f584e43fd6148985929da8d1
ravinderchautala <ryvinder@gmail.com>
2022-07-14 12:22:11 +0530

    create methods tutorial completed

---
+++ yahoo_baba/ch_72_create-methods/script.js
@@ -0,0 +1,9 @@
+var newElement = document.createElement("p");
+
+var newText = document.createTextNode("This is just text");
+
+var newComment = document.createTextNode("This is just comment");
+
+console.log(newElement);
+console.log(newText);
+console.log(newComment);
