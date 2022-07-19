57f568edb950f5f7590b2a70d1b4adc6be6fd864
ravinderchautala <ryvinder@gmail.com>
2022-07-19 20:01:29 +0530

    tutorial 78 completed

---
+++ yahoo_baba/ch_78_hasAttribute_hasChildNodes/script.js
@@ -0,0 +1,7 @@
+// var target = document.getElementById("outer");
+var target = document.getElementById("child-3");
+
+// var find = target.hasAttribute("style");
+var find = target.hasChildNodes();
+
+console.log(find);
