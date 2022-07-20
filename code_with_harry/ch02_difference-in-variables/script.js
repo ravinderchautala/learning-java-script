80f5401fe2e97029dedc2636277b4adb6db65f4d
ravinderchautala <ryvinder@gmail.com>
2022-07-20 12:33:15 +0530

    variables tutorial completed

---
+++ code_with_harry/ch02_difference-in-variables/script.js
@@ -0,0 +1,15 @@
+var a = 45;
+let b ="Ravinder";
+let c = null;
+let d = undefined;
+const author = "Ravinder";
+// author = 5;  --> throws an error because constant can not be changed
+
+{
+    var a = 56;
+    console.log(a);
+    let b ="Chautala";
+    console.log(b);
+}
+console.log(a);
+console.log(b);
