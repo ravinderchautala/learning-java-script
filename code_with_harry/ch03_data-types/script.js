78a3665c8227ae276ada709a4d4116e334be52c3
ravinderchautala <ryvinder@gmail.com>
2022-07-20 12:47:14 +0530

    data typess tutorial completed

---
+++ code_with_harry/ch03_data-types/script.js
@@ -0,0 +1,18 @@
+// nn ss bb u -Premitive in Js
+let a = null;
+let b = 345;
+let c = true;
+let d = BigInt("557");
+let e = "Ravinder";
+let f = Symbol("I am a symbol");
+let g = undefined;
+console.log(a,b,c,d,e,f,g);
+console.log(typeof e)
+
+// objects in Js
+const item ={
+    name: "Ravinder",
+    roll: BigInt("1911035"),
+    age: 23
+};
+console.log(item["roll"]);
