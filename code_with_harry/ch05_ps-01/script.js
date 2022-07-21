0f8626472af232e5ece1293f110cb3e371873fc4
ravinderchautala <ryvinder@gmail.com>
2022-07-21 09:38:00 +0530

    practice set 1 completed

---
+++ code_with_harry/ch05_ps-01/script.js
@@ -0,0 +1,20 @@
+// Q-1
+let a = "Ravinder";
+let b = 35;
+ 
+console.log(a + b);
+
+// Q-2
+console.log(typeof (a + b));
+
+// Q-3
+const c ={
+    name: "Ravinder",
+    section: 1,
+    rollno : 35
+};
+// c = "Vinita";
+
+// Q-4
+c["friend"] ="Vinita";
+console.log(c);
