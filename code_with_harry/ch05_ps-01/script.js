3e63473bd9cfb2fc305be2fe57896d6cfab9f976
ravinderchautala <ryvinder@gmail.com>
2022-07-21 15:22:09 +0530

    empty  commit

--- code_with_harry/ch05_ps-01/script.js
+++ code_with_harry/ch05_ps-01/script.js
@@ -1,20 +1,29 @@
 // Q-1
 let a = "Ravinder";
 let b = 35;
- 
+
 console.log(a + b);
 
 // Q-2
 console.log(typeof (a + b));
 
 // Q-3
-const c ={
+const c = {
     name: "Ravinder",
     section: 1,
-    rollno : 35
+    rollno: 35
 };
 // c = "Vinita";
 
 // Q-4
-c["friend"] ="Vinita";
+c["friend"] = "Vinita";
 console.log(c);
+
+// Q-5
+const dict = {
+    facedown: "with the face or the front or upper surface downward",
+    facticity: "the condition or quality of being a fact",
+    faddist: "a person following a fad",
+    abjective :"tending to degrade, humiliate, or demoralize",
+    abneural: "Away from the neural axis"
+};
