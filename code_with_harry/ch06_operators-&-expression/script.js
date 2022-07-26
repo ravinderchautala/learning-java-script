45b51b026a909a783fca983798dc3fdf7ac2531a
ravinderchautala <ryvinder@gmail.com>
2022-07-27 01:28:46 +0530

    operators tutorial completed

---
+++ code_with_harry/ch06_operators-&-expression/script.js
@@ -0,0 +1,31 @@
+let a = 10;
+let b =4;
+// airthmathic operators
+console.log("a+b=",a+b);
+console.log("a-b=",a-b);
+console.log("a*b=",a*b);
+console.log("a/b=",a/b);
+console.log("a%b=",a%b);
+console.log("a**b=",a**b);
+console.log("a++=",a++);
+console.log(a);
+console.log("a--=",a--);
+console.log(a);
+console.log("++a=",++a);
+console.log(a);
+console.log("--a=",--a);
+console.log(a);
+
+// assignment operators
+let c = 5;
+
+c +=5;//c=c+5
+console.log(c);
+c -=5; //c=c-5
+console.log(c);
+c *=5; //c=c*5
+console.log(c);
+c /=5; //c=c/5
+console.log(c);
+c**=b; //c=c**b
+console.log(c);
