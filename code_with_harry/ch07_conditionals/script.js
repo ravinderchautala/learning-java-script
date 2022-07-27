2254371946d4322c27eac321ca0dabe155d28256
ravinderchautala <ryvinder@gmail.com>
2022-07-27 11:54:00 +0530

    file updated

--- code_with_harry/ch07_conditionals/script.js
+++ code_with_harry/ch07_conditionals/script.js
@@ -1,28 +1,29 @@
-// let a = prompt("Enter day number");
-// a = Number.parseInt(a);
+let a = prompt("Enter day number");
+a = Number.parseInt(a);
 
-// /*if(a>18){
-//     alert("You're eligible for voting.")
-// }else{
-//     alert("You're not eligible for voting.")
-// }*/
-// switch(a){
-//     case 1: alert("Monday")
-//     break
-//     case 2: alert("Tuesday")
-//     break
-//     case 3: alert("Wednesday")
-//     break
-//     case 4: alert("Thrusday")
-//     break
-//     case 5: alert("Friday")
-//     break
-//     case 6: alert("Saturday")
-//     break
-//     case 7: alert("Sunday")
-//     break
-//     default : alert("Please enter a valid number.")
-// }
+/*if(a>18){
+    alert("You're eligible for voting.")
+}else{
+    alert("You're not eligible for voting.")
+}*/
+switch(a){
+    case 1: alert("Monday")
+    break
+    case 2: alert("Tuesday")
+    break
+    case 3: alert("Wednesday")
+    break
+    case 4: alert("Thrusday")
+    break
+    case 5: alert("Friday")
+    break
+    case 6: alert("Saturday")
+    break
+    case 7: alert("Sunday")
+    break
+    default : alert("Please enter a valid number.")
+}
+
 // ternary operator
 let b = prompt("Do you like sports?")
 alert(b == "no"? 'Very bad':'Nice')
