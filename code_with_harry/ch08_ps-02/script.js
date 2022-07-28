182d8a957015363af644f4f4d30ac18b9a13d529
ravinderchautala <ryvinder@gmail.com>
2022-07-28 22:16:31 +0530

    question 1 is completed

---
+++ code_with_harry/ch08_ps-02/script.js
@@ -0,0 +1,8 @@
+// ps 02
+// Ques-1
+let age = prompt("Enter your age")
+if(age>10 && age<20){
+    alert("Yes you pass the eligibility criteria.")
+}else {
+    alert("Sorry you are not eligibile.")
+}
