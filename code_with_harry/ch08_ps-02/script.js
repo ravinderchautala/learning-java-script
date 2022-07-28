cecd735d53671b9078caf709327c0dc030cd427f
ravinderchautala <ryvinder@gmail.com>
2022-07-28 22:53:11 +0530

    Practice 2 is completed

--- code_with_harry/ch08_ps-02/script.js
+++ code_with_harry/ch08_ps-02/script.js
@@ -1,8 +1,39 @@
 // ps 02
 // Ques-1
-let age = prompt("Enter your age")
-if(age>10 && age<20){
+/*let age = prompt("Enter your age")
+if (age > 10 && age < 20) {
     alert("Yes you pass the eligibility criteria.")
-}else {
+} else {
     alert("Sorry you are not eligibile.")
+}*/
+// Ques - 2
+/*a = parseInt(a);
+let a = prompt("Enter a day number")
+switch (a) {
+    case 1: alert("Monday")
+        break
+    case 2: alert("Tuesday")
+        break
+    case 3: alert("Wednesday")
+        break
+    case 4: alert("Thrusday")
+        break
+    case 5: alert("Friday")
+        break
+    case 6: alert("Saturday")
+        break
+    case 7: alert("Sunday")
+        break
+    default: alert("Please enter a valid number.")
+}*/
+// Ques-3
+let a = prompt("Enter a number")
+a = parseInt(a)
+if(a%2==0){
+    alert("Number is divisible by 2.")
+} else if(a%3==0){
+    alert("Number is divisible by 3.")
+} else{
+    alert("Number is nor divisible by 2 and neither divisible by 3.")
+
 }
