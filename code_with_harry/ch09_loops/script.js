e06383ce3ec83858aeb8f8647b33475f28f4ede9
ravinderchautala <ryvinder@gmail.com>
2022-07-30 15:50:07 +0530

    for loop tutorial completed

---
+++ code_with_harry/ch09_loops/script.js
@@ -0,0 +1,18 @@
+// for loop
+for(let i=0;i<50;i++){
+    console.log(i);
+}
+let obj ={
+    ravinder: 45,
+    vinita: 99,
+    jatta: 40,
+    Anu: 100
+}
+// for in loop
+for(let b in obj){
+    console.log("marks of "+ b +" are "+obj[b])
+}
+// for of loop
+for( let c of "Vinita"){
+    console.log(c)
+}
