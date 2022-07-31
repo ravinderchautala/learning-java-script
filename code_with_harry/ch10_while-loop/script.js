ceeae433f8d190bd87532a59103f7d918dea15bb
ravinderchautala <ryvinder@gmail.com>
2022-07-31 21:45:32 +0530

    while loop and do while loop tutorial completed

---
+++ code_with_harry/ch10_while-loop/script.js
@@ -0,0 +1,16 @@
+let a = prompt("Enter a number:");
+a =Number.parseInt(a);
+let i = 0;
+// While loop
+
+// while(i<a){
+//     console.log(i);
+//     i++;
+// }
+
+
+// Do While loop
+do{
+    console.log(i);
+    i++;
+}while(i<a);
