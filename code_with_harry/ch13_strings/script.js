e039101d2b192bb52b33a70780367732085e8d41
ravinderchautala <ryvinder@gmail.com>
2022-08-02 22:25:05 +0530

    strings tutorial completed

---
+++ code_with_harry/ch13_strings/script.js
@@ -0,0 +1,10 @@
+let name = "Vinita"
+let lname = 'Ravinder'
+// console.log(name + lname)
+// templete literals
+let rel = `${name} is love of ${lname}`
+console.log(rel)
+// Escape Sequence Charactors
+// let fname = 'Ravinder\'Vinita'
+let fname = "Ravinder\"Vinita"
+console.log(fname)
