9f5cdab2346a8db01ec5c3ee8e3cd6f5d8364f38
ravinderchautala <ryvinder@gmail.com>
2022-08-03 21:57:59 +0530

    strings mathods tutorial completed

---
+++ code_with_harry/ch14_strings-methods/script.js
@@ -0,0 +1,9 @@
+let fname = "Ravinder Vinita"
+
+console.log(fname.length)
+
+console.log(fname.toUpperCase())
+console.log(fname.toLowerCase())
+console.log(fname.slice(3,7))
+let newname = fname.replace("Ravinder", "Jatta")
+console.log(newname)
