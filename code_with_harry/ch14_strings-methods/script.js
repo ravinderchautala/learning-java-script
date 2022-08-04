7c29bdddc0de880dee2571b30d8ca3a2c9062628
ravinderchautala <ryvinder@gmail.com>
2022-08-04 22:23:17 +0530

    strings tutorial completed

--- code_with_harry/ch14_strings-methods/script.js
+++ code_with_harry/ch14_strings-methods/script.js
@@ -7,3 +7,8 @@ console.log(fname.toLowerCase())
 console.log(fname.slice(3,7))
 let newname = fname.replace("Ravinder", "Jatta")
 console.log(newname)
+let trimname = "     Vinita      "
+
+console.log(trimname)
+console.log(trimname.trim())
+console.log(fname[9])
