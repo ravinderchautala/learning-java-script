c3bb68017ff94bb5059c965a426ea7646da31666
ravinderchautala <ryvinder@gmail.com>
2022-08-05 02:12:48 +0530

    practice set 4 tutorial completed

---
+++ code_with_harry/ch15_ps-04/script.js
@@ -0,0 +1,19 @@
+// Ques 1
+console.log("Vin\"".length)
+
+// Ques 2
+let name ="Vinita Ravinder"
+console.log(name.includes("Vinita"))
+console.log(name.endsWith("Vinita"))
+console.log(name.startsWith("Vinita"))
+
+// Ques 3
+console.log(name.toLowerCase())
+
+// Ques 4
+let sen = "Please give Rs.1000"
+console.log(sen.slice(12))
+
+// Ques 5
+let newName =name.replace("Ravinder","Jatta")
+console.log(newName)
