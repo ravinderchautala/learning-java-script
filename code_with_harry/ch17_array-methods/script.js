a0c522b17282248a5363606dbdf708178c640f74
ravinderchautala <ryvinder@gmail.com>
2022-08-07 13:05:19 +0530

    array methods tutorial conoleted

---
+++ code_with_harry/ch17_array-methods/script.js
@@ -0,0 +1,23 @@
+let num = [1, 2, 3, 4, 5]
+
+let b = num.toString();
+console.log(b)
+console.log(typeof b)
+
+let c = num.join("^")
+console.log(c)
+console.log(typeof c)
+
+let pop = num.pop()
+console.log(pop)
+console.log(num)
+
+let push = num.push(10)
+console.log(num)
+
+let shift = num.shift()
+console.log(shift , num)
+
+let unshift = num.unshift(8)
+console.log(unshift,num)
+
--- yahoo_baba/ch_85_window_height_and_width/script.js
+++ yahoo_baba/ch_85_window_height_and_width/script.js
@@ -1,14 +1,6 @@
-// let ih = window.innerHeight;
-// let iw = window.innerWidth;
-// let oh = window.outerHeight;
-// let ow= window.outerWidth;
-// console.log(ih,oh);
-// console.log(iw,ow);
-function add(a){
-    if(a>0){
-    console.log(a)
-    add(a-1)
-}
-}
-let x=3;
-add(x)
+let ih = window.innerHeight;
+let iw = window.innerWidth;
+let oh = window.outerHeight;
+let ow= window.outerWidth;
+console.log(ih,oh);
+console.log(iw,ow);
