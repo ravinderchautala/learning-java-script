be93e88e3a978d441d26f3cb160a7704efa40ade
ravinderchautala <ryvinder@gmail.com>
2022-08-08 22:48:15 +0530

    window open and close methods tutorial completed

---
+++ yahoo_baba/ch_86_window-open-&-close/script.js
@@ -0,0 +1,7 @@
+let myWindow;
+function openWindow(){
+    myWindow = window.open("https://www.youtube.com/watch?v=Z0DmSWJyunc","_blank","width=500px,height=300px,left=500px,top=300px")
+}
+function closeWindow(){
+   myWindow.close()
+}
