e5a09f31b173fefc1571a8ed509983dab3e9a6dd
ravinderchautala <ryvinder@gmail.com>
2022-08-12 17:03:06 +0530

    javascript variables

--- john_smilga/ch02_variable/app.js
+++ john_smilga/ch02_variable/app.js
@@ -1,5 +1,11 @@
-let a = "Anu";
-a = "Vinita"
-let hname;
-hname = " Ravinder"
+// using var
+var v = "Vinita"
+
+// Using let
+let a = "Anu"
+
+// using const
+const hname = "Ravinder"
+
+console.log(v + hname)
 console.log(a + hname)
