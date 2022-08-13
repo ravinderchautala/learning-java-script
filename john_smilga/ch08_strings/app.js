b3fa10b781ee95e19a4f845d66db14afe840f4b3
ravinderchautala <ryvinder@gmail.com>
2022-08-13 14:28:33 +0530

    string methods tutorial completed

---
+++ john_smilga/ch08_strings/app.js
@@ -0,0 +1,15 @@
+let text =" Vinita Ravinder"
+let result = text.length
+console.log(result)
+
+console.log(text.length)
+console.log(text.toLowerCase())
+console.log(text.toUpperCase())
+console.log(text.charAt(0))
+console.log(text.charAt(text.length-1))
+console.log(text.indexOf('n'))
+console.log(text.trim())
+console.log(text.startsWith(" Vinita"))
+console.log(text.endsWith("er"))
+console.log(text.includes("Vinita"))
+console.log(text.slice(1,7))
