06c00fb046535128564a4351cae7dc845a6bcf05
ravinderchautala <ryvinder@gmail.com>
2022-08-13 14:10:58 +0530

    loops tutorial completed

---
+++ john_smilga/ch07_loops/app.js
@@ -0,0 +1,20 @@
+// while loop
+
+let amount=10
+while(amount>0){
+    console.log("i have "+ amount +"$")
+    amount--
+}
+
+// do while loop
+let money = 0
+ do{
+    console.log('You have '+money+' dollars')
+    money++
+ }while(money<10)
+
+//  for loop
+let i;
+for(i=0;i<10;i++){
+    console.log("Vinita")
+}
