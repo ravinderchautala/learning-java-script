08af82d479a56c400997884840c003541f7d4650
ravinderchautala <ryvinder@gmail.com>
2022-08-20 00:14:02 +0530

    local scope in js

---
+++ john_smilga/ch17_local-scope/app.js
@@ -0,0 +1,16 @@
+let name = "Vinita"
+
+
+// console.log(`Her name is ${name}`);
+
+function call(){
+    const name = "Annu"
+    console.log(name);
+}
+call()
+
+if(true){
+    const name = "Viniya"
+    console.log(name)
+}
+console.log(name)
