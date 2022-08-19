fe8f569d185e77a6ea5ad0c9f7d0180761504f14
ravinderchautala <ryvinder@gmail.com>
2022-08-20 00:00:34 +0530

    global scope

--- john_smilga/ch16_global-scope/app.js
+++ john_smilga/ch16_global-scope/app.js
@@ -1,9 +1,21 @@
 let name = "Vinita"
 name = "Annu"
-console.log(`Her name is ${name}`);
+
+// console.log(`Her name is ${name}`);
 
 function call(){
     console.log(name);
     name = "Vinita"
+    function inner(){
+        name = "inner name value"
+        console.log(`This value from inner function ${name}`)
+    }
+    inner()
 }
 call()
+
+if(true){
+    console.log(name);
+    name = "Annu"
+}
+console.log(name)
