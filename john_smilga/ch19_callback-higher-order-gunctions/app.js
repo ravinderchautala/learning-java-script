acb62b31cf50dd68557c241635ecc682233a3637
ravinderchautala <ryvinder@gmail.com>
2022-08-20 00:45:28 +0530

    Higher order and callback functions in js

---
+++ john_smilga/ch19_callback-higher-order-gunctions/app.js
@@ -0,0 +1,19 @@
+// callback functions
+function morning(name){
+    return `Good Morning ${name}`
+}
+function afterNoon(name){
+    return `Good Afternoon ${name}`
+}
+function evening(name){
+    return `Good Evening ${name}`
+}
+
+// global function - higher order function bcz its takes a function as an argument and also return a function
+function greet(name,cb){
+    const myName = "Ravinder"
+    console.log(`${cb(name)} ,My name is ${myName}`)
+}
+greet("Vinita",morning)
+greet("Vinita",afterNoon)
+greet("Vinita",evening)
