de017ebe0d00d2c18ce36646dd9b9cd98f1959f1
ravinderchautala <ryvinder@gmail.com>
2022-08-22 00:53:53 +0530

    exercise bug fixed

--- code_with_harry/ch22_exercise/app.js
+++ code_with_harry/ch22_exercise/app.js
@@ -1,13 +1,19 @@
 let ran = (max) => {
     return Math.floor(Math.random() * max)
 }
+let b =ran(100)
 let a = prompt("Guess the numner")
 a = Number.parseInt(a)
-for(let i = 1;i<=10;i++){
-let score = 10-i
-if(ran(10) == a){
+for(let i = 1;i<=100;i++){
+let score = 100-i
+if(b == a){
+    document.write(`You have left ${score} left and the original number is ${b}`)
     console.log(`Your Score is : ${score}`)
     break
-}else{
+}else if(b > a){
+    alert("your number is smaller")
+    a = prompt("Guess the numner")
+}else if(b < a){
+    alert("Your number is greater")
     a = prompt("Guess the numner")
 }}
