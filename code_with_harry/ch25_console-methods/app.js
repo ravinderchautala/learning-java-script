247208ccd2cbadef75690ca3a32721d63c2c00ba
ravinderchautala <ryvinder@gmail.com>
2022-08-21 10:34:25 +0530

    console methods tut completed

---
+++ code_with_harry/ch25_console-methods/app.js
@@ -0,0 +1,11 @@
+console.time("a")
+console.log(console);
+console.log("Hi Vinita")
+console.error("This is error")
+console.assert(5>6)
+console.warn("This is warning")
+// console.clear()
+const obj = {a:1,b:2,c:3}
+console.table(obj)
+console.info("This is info")
+console.timeEnd("a")
