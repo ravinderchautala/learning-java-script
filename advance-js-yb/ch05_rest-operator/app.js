1e5f40337f2a97acfee2f8711e4770b6e00ae642
ravinderchautala <ryvinder@gmail.com>
2022-08-26 22:32:29 +0530

    rest operator

--- advance-js-yb/ch05_rest-operator/app.js
+++ advance-js-yb/ch05_rest-operator/app.js
@@ -8,12 +8,12 @@
 // sum("Vinita",23,45,35,46)
 
 // rest operator (...args)
-function sum(name,...args){
+function sum(name,course,...args){
     let sum = 0
     for(let i in args){
         sum += args[i]
     }
     console.log(sum);
-    console.log(name)
+    console.log(name,course)
 }
-sum("Vinita",23,45,35,46)
+sum("Vinita","P.hd",23,40,34,46)
