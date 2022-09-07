41fb1aa9d2333bd707803932eb18036521d70209
ravinderchautala <ryvinder@gmail.com>
2022-09-07 06:22:05 +0530

    practice set 6 completed

---
+++ code_with_harry/ch29_ps-06/app.js
@@ -0,0 +1,41 @@
+// // Ques 1
+// let age = prompt("Enter your age")
+// age = Number.parseInt(age)
+// let a = 18- age
+// if(age<18 && age > 0){
+//     alert("You can't drive wait for "+ a + " years to be eligible")
+// }else if(age>18){
+//     alert("You can drine!!!!")
+// }
+
+//  // Ques 2
+// function confrm() {
+//   let age = prompt("Enter your age");
+//   age = Number(age);
+//   let a = 18 - age;
+//   if (age < 18) {
+//     alert("You can't drive  wait for " + a + " years to be eligible");
+//   } else {
+//     alert("You can drine!!!!");
+//   }
+
+//   if (confirm("Do you want to see prompt box again")) {
+//     confrm();
+//   }
+// }
+// confrm();
+
+// // Ques 3
+// if(age<0){
+//     console.error("Enter a Valid age")
+// }
+ 
+// Ques 4
+// let a = prompt("Enetr a number")
+// if(a>4){
+//     location.href = "https://www.google.com"
+// }
+
+// Ques 5
+let color = prompt("Enetr the page background color")
+document.body.style.background = color
