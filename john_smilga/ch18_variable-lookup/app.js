3a80aa429b83300dc3bab118d997650c33195ce7
ravinderchautala <ryvinder@gmail.com>
2022-12-27 00:51:16 +0530

    variables scopes look up tutorial completed

--- john_smilga/ch18_variable-lookup/app.js
+++ john_smilga/ch18_variable-lookup/app.js
@@ -3,15 +3,15 @@
 const globalNumber = 5
 
 
-function add(num1,num2){
-    const globalNumber =20
+function add(num1, num2) {
+    const globalNumber = 20
     const result = num1 + num2 + globalNumber
-    function mul(){
-       const globalNumber = 100
+    function mul() {
+        const globalNumber = 100
         console.log(result * globalNumber);
     }
     mul()
     return result
 }
-let c = add(3,4)
+let c = add(3, 4)
 console.log(c);
