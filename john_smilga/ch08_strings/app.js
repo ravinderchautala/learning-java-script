801fca21c4ef906993865b7125f0a0fab6d069ad
ravinderchautala <ryvinder@gmail.com>
2022-12-26 18:59:31 +0530

    strings in js tutorial completed

--- john_smilga/ch08_strings/app.js
+++ john_smilga/ch08_strings/app.js
@@ -1,15 +1,17 @@
-let text =" Vinita Ravinder"
-let result = text.length
-console.log(result)
+let text = " Vinita Ravinder   ";
+console.log(text);
 
-console.log(text.length)
-console.log(text.toLowerCase())
-console.log(text.toUpperCase())
-console.log(text.charAt(0))
-console.log(text.charAt(text.length-1))
-console.log(text.indexOf('n'))
-console.log(text.trim())
-console.log(text.startsWith(" Vinita"))
-console.log(text.endsWith("er"))
-console.log(text.includes("Vinita"))
-console.log(text.slice(1,7))
+let res = text.length;
+console.log(res);
+
+console.log(text.length);
+console.log(text.toUpperCase());
+console.log(text.toLowerCase());
+console.log(text.charAt(1));
+console.log(text.indexOf('a'));
+console.log(text.trim());
+text.toLowerCase();
+console.log(text.trim().toLowerCase().startsWith('vini'));
+console.log(text.includes('Vini'));
+let str = text.slice(0, 7);
+console.log(str);
