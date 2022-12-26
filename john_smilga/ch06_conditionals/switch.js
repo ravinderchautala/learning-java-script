4e087d976640c07058ff07de85995f635ecd4b77
ravinderchautala <ryvinder@gmail.com>
2022-12-26 16:10:33 +0530

    switch case statement tutorial completed

---
+++ john_smilga/ch06_conditionals/switch.js
@@ -0,0 +1,17 @@
+const dice = 2;
+
+switch (dice) {
+    case 1: console.log("you get one");
+        break;
+    case 2: console.log("you get two");
+        break;
+    case 3: console.log("you get three");
+        break;
+    case 4: console.log("you get four");
+        break;
+    case 5: console.log("you get five");
+        break;
+    case 6: console.log("you get six");
+        break;
+    default: console.log("you didn't roll dice properly");
+}
