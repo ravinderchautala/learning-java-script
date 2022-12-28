a74962fd394953bd5d4f568735bf742645739eba
ravinderchautala <ryvinder@gmail.com>
2022-12-28 15:31:03 +0530

    math function tut completed

---
+++ john_smilga/ch25_math/app.js
@@ -0,0 +1,22 @@
+// const number = 4.56735274
+// const res = Math.floor(number)
+// const res = Math.ceil(number)
+
+
+
+// const number = 25
+// const res = Math.sqrt(number)
+
+// const res = Math.PI
+
+
+// const res = Math.min(1, 2, 34, 25, 0)
+
+
+// const res = Math.max(1, 2, 34, 25, 0)
+
+
+// const res = Math.random() * 10;
+
+const res = Math.ceil(Math.random() * 10);
+console.log(res);
