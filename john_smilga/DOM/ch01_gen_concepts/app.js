c5449ac1268fb19a9f2687a2af3bda01b1ee73a0
ravinderchautala <ryvinder@gmail.com>
2022-12-28 23:17:32 +0530

    get elements by id tut completed

---
+++ john_smilga/DOM/ch01_gen_concepts/app.js
@@ -0,0 +1,13 @@
+// getElemenysById
+const h1 = document.getElementById('title');
+h1.style.color = 'red';
+
+
+const btn = document.getElementById('btn');
+btn.style.color = "white";
+btn.style.backgroundColor = 'aqua';
+
+
+// document.getElementById('btn').style.backgroundColor = 'aqua';
+
+// document.getElementById('btn').style.color = 'white';
