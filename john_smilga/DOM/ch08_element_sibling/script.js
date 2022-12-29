3ec3bcfe4fe4bc40ebb9ae614299fa02240b9d23
ravinderchautala <ryvinder@gmail.com>
2022-12-29 21:58:32 +0530

    element sibling prop completed

---
+++ john_smilga/DOM/ch08_element_sibling/script.js
@@ -0,0 +1,8 @@
+// next element sibling
+const first = document.querySelector('.first');
+first.nextElementSibling.style.color = 'orange';
+
+// previous element sibling
+const last = document.querySelector('.last');
+last.previousElementSibling.style.color = 'brown';
+
