5668dff9fd0bf6c138b6ce87d30e35c47fdf65a4
ravinderchautala <ryvinder@gmail.com>
2022-12-30 14:13:45 +0530

    nodeValue and textContent prop tut completed

---
+++ john_smilga/DOM/ch09_textContentVsnodeValue/script.js
@@ -0,0 +1,6 @@
+const heading = document.getElementById('special');
+const text = heading.nodeValue;
+// node value
+console.log(heading.childNodes[0].nodeValue);
+// text content
+console.log(heading.textContent);
