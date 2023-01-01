f87d1b71c40fcf8c5d668d95d1d41cc5db00acce
ravinderchautala <ryvinder@gmail.com>
2023-01-01 15:18:05 +0530

    css style changing tutorial completed

---
+++ john_smilga/DOM/ch18_CSS/script.js
@@ -0,0 +1,9 @@
+// CSS
+
+const random = document.querySelector('#random');
+
+// random.style.backgroundColor = 'blue';
+// random.style.color = 'white';
+// random.style.fontSize = '2rem';
+// random.style.textTransform = 'uppercase';
+random.classList.add('title');
