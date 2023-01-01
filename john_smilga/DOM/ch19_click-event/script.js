a7899089dc2a418ba009d05cfbc9a1cfce0e6849
ravinderchautala <ryvinder@gmail.com>
2023-01-01 15:56:42 +0530

    click events tutorial completed

---
+++ john_smilga/DOM/ch19_click-event/script.js
@@ -0,0 +1,9 @@
+// select element
+// addeventlistener
+// add event, what to do
+
+const btn = document.querySelector('.btn');
+const heading = document.querySelector('h2');
+btn.addEventListener('click', function () {
+    heading.classList.add('red');
+})
