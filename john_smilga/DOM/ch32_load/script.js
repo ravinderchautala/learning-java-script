875a7eb1abcf9006989dff9b28548f6c0377f441
ravinderchautala <ryvinder@gmail.com>
2023-01-05 01:26:24 +0530

    load event tut completed

--- john_smilga/DOM/ch32_load/script.js
+++ john_smilga/DOM/ch32_load/script.js
@@ -1,4 +1,16 @@
+
+
+
+
 window.addEventListener('load', function () {
+    console.log('i will run second');
     const image = document.querySelector('img');
     console.log(image);
+    console.log(image.width);
+})
+window.addEventListener('DOMContentLoaded', function () {
+    console.log('i will run first');
+    const image = document.querySelector('img');
+    console.log(image);
+    console.log(image.width);
 })
