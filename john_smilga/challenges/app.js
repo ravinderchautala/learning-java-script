ae94c2d072f69437df6bbf340aa04b7f6816451c
ravinderchautala <ryvinder@gmail.com>
2024-03-08 17:27:37 +0530

    challenge 5,6 and 7 completed

--- john_smilga/challenges/app.js
+++ john_smilga/challenges/app.js
@@ -93,3 +93,30 @@ console.log(car.make);
 console.log(car.colors[0]);
 car.drive();
 car.stop();
+
+/*
+============
+Challenge-7
+============
+*/
+const person1 = {
+    name: "Ravinder",
+    age: 20,
+    status: "single"
+}
+const person2 = {
+    name: "Jatta",
+    age: 23,
+    status: "Committed"
+}
+
+if (person1.age > 18 && person1.status === "single") {
+    console.log("you are eligible");
+} else {
+    console.log("you are not eligible");
+}
+if (person2.age > 18 && person2.status === "single") {
+    console.log("you are eligible");
+} else {
+    console.log("you are not eligible");
+}
