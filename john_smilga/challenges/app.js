f611dbceeaecaf763189e3767464f7c965881b65
ravinderchautala <ryvinder@gmail.com>
2024-03-08 11:37:03 +0530

    challenge 1 and 2 completed

--- john_smilga/challenges/app.js
+++ john_smilga/challenges/app.js
@@ -1,145 +1,24 @@
-// 1st challenge -Variables
-let firstName = "Vinita";
-let lastName = "Ravinder";
-let address = "main street";
-address = "first street";
+/*
+============
+Challenge-1
+============
+*/
+let firstName = "Ravinder";
+let lastName = "Chautala";
+let address = "Main Street";
+address = "First Street";
+
 console.log(firstName);
 console.log(lastName);
 console.log(address);
 
-// 2nd challenge - Concating Strings
-let street = "Bas Gokal";
-let country = "India";
+/*
+============
+Challenge-2
+============
+*/
+let street, country;
+street = "Ambedkar street";
+country = "India";
 let fullMailingAddress = street + " " + country;
 console.log(fullMailingAddress);
-
-
-//  3rd challenge - numbers
-let score1, score2, score3, totalScore, averageScore;
-score1 = 97;
-score2 = 98;
-score3 = 95;
-totalScore = score1 + score2 + score3;
-averageScore = totalScore / 3;
-console.log(totalScore);
-console.log(averageScore);
-
-
-
-// 4th challenge - Arrays
-const fruits = ['Mango', 'Apple', 'Banana', 'Pineapple', 'Orange', 9];
-
-let firstFruit = fruits[0];
-
-fruits[fruits.length - 1] = 'Guava';
-
-console.log(firstFruit, fruits);
-
-
-// 5th challenge - Functions
-// function calculateTotal(subTotal, tax) {
-//     return subTotal + tax;
-// }
-const calculateTotal = function (subTotal, tax) {
-    return subTotal + tax;
-}
-
-const order1 = calculateTotal(100, 20);
-const order2 = calculateTotal(90, 18);
-const order3 = calculateTotal(150, 30);
-console.log(order1, order2, order3);
-
-// 6th challenge - Objects
-
-const car = {
-    make: "Ferrari",
-    model: '812 GTS',
-    year: 2022,
-    colors: ['Red', 'Yelllow', 'Blue', 'Grey'],
-    hybrid: false,
-    drive: function () {
-        console.log('hey do you want to take a test drive');
-    },
-    stop: function () {
-        console.log('please stop the car if engine make different noise');
-    }
-};
-console.log(car.make);
-console.log(car.colors[0]);
-car.drive();
-car.stop();
-
-// 7th challenge - Conditionals
-const person1 = {
-    name: 'Vinita',
-    age: 25,
-    status: 'resident',
-
-}
-const person2 = {
-    name: 'Annu',
-    age: 23,
-    status: 'tourist',
-}
-
-if (person1.age >= 18 && person1.status === 'resident') {
-    console.log("you are eligible to marry with me");
-} else {
-    console.log("you are not eligible to marry with me");
-}
-if (person2.age >= 18 && person2.status === 'resident') {
-    console.log("you are eligible to marry with me");
-} else {
-    console.log("you are not eligible to marry with me");
-}
-
-// 8th challenge - Strings
-function fullName(firstName, lastName) {
-    const value = `hey ${firstName} ${lastName} this is Ravinder here`;
-    console.log(value.toUpperCase());
-}
-fullName('Vinita', 'Rahar');
-
-// 9th challenge - Arrays methods
-
-const students = [{ id: 1, name: 'Vinita', score: 100, subject: 'Maths' }, { id: 2, name: 'Ravinder', score: 89, subject: 'coding' }, { id: 3, name: 'Annu', score: 100, subject: 'Maths' }, { id: 4, name: 'Jatta', score: 85, subject: 'java' }]
-console.log(students);
-
-// map method
-const updatedStudents = students.map(function (student) {
-    student.role = 'student';
-    return student;
-})
-console.log(updatedStudents);
-
-// filter method
-const filterStudents = students.filter(function (student) {
-    return student.score >= 90;
-})
-console.log(filterStudents);
-
-// find method
-const specific = students.find(function (student) {
-    return student.id === 3;
-})
-console.log(specific);
-
-// reduce method
-const avgScore = students.reduce(function (acc, item) {
-
-    return acc + item.score;
-}, 0) / students.length;
-
-console.log(avgScore);
-
-const survey = students.reduce(function (survey, student) {
-    const favSubject = student.subject;
-    if (survey[favSubject]) {
-        survey[favSubject] += 1;
-    } else {
-        survey[favSubject] = 1;
-    }
-    return survey
-}, {})
-
-console.log(survey);
