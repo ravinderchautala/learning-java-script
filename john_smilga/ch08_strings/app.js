b17efa7227a5f20318036a6df08b0918f1337b70
ravinderchautala <ryvinder@gmail.com>
2024-03-09 10:49:44 +0530

    string methods completed

--- john_smilga/ch08_strings/app.js
+++ john_smilga/ch08_strings/app.js
@@ -1,17 +1,15 @@
-let text = " Vinita Ravinder   ";
+let text = " Ravinder Chautala  ";
+
 console.log(text);
-
-let res = text.length;
-console.log(res);
-
 console.log(text.length);
-console.log(text.toUpperCase());
 console.log(text.toLowerCase());
-console.log(text.charAt(1));
-console.log(text.indexOf('a'));
+console.log(text.toUpperCase());
+console.log(text.charAt(0));
+console.log(text.charAt(text.length - 1));
+console.log(text.indexOf('R'));
 console.log(text.trim());
-text.toLowerCase();
-console.log(text.trim().toLowerCase().startsWith('vini'));
-console.log(text.includes('Vini'));
-let str = text.slice(0, 7);
-console.log(str);
+console.log(text.startsWith('ravinder'));
+console.log(text.trim().toLowerCase().startsWith('ravinder'));
+console.log(text.includes('avi'));
+console.log(text.slice(2, 8));
+console.log(text.substring(1, 5));
